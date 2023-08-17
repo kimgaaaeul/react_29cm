a6a54fdea31fe7cc351b3f74b576f7f57ad500b0
kimgaaaeul <126546237+kimgaaaeul@users.noreply.github.com>
2023-08-17 13:52:02 +0900

    Add files via upload

---
+++ src/showCaseData.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    "id":0,
+    "thumbnail":"../img/feed_main_post.jpg",
+    "header_img":"../img/showcase_4_1.jpg",
+    "first_img": "../img/showcase_4_2.jpg",
+    "event_item":"../img/showcase_4_3.jpg",
+    "img_box_1":"../img/showcase_4_4.jpg",
+    "img_box_2":"../img/showcase_4_5.jpg",
+    "img_box_3":"../img/showcase_4_6.jpg",
+    "img_box_4":"../img/showcase_4_7.jpg",
+    "lookbook_1":"../img/showcase_4_8.jpg",
+    "lookbook_2":"../img/showcase_4_9.jpg",
+    "lookbook_3":"../img/showcase_4_10.jpg",
+    "rec_item_1":"../img/showcase_rec_1_1.jpg",
+    "rec_item_2":"../img/showcase_rec_1_2.jpg",
+    "rec_item_3":"../img/showcase_rec_2_1.jpg",
+    "rec_item_4":"../img/showcase_rec_2_2.jpg"
+  }
+]
